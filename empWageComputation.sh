38942bfb6eb784576b729c280559c4289d1b0210
naincyrani06 <naincyrani06it@gmail.com>
2020-08-10 20:59:01 +0530

    Calculating Employee Wage Using Switch Case Statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,24 +1,26 @@
 #!/bin/bash -x
 empCheck=$((RANDOM%3))
 
-isPartTime=1
-isFullTime=2
+isPartTime=2
+isFullTime=1
 empWagePerHour=20
 
 empHr=0
 
-if [ $empCheck -eq $isFullTime ]
-then
-	echo "Employee is FullTime"
-	empHr=8
-elif [ $empCheck -eq $isPartTime ]
-then
-	echo "Employee is PartTime"
-	empHr=4
-
-else
-	echo "Employee is Absent"
-	empHr=0
-fi
+case $empCheck in
+       $isFullTime)
+                echo "Employee is FullTime"
+                empHr=8
+       ;;
+       $isPartTime)
+               echo "Employee is PartTime"
+               empHr=4
+       ;;
+       *)
+               echo "Employee is Absent"
+               empHr=0
+       ;;
+esac
 
 echo "Employee's Daily Wage: "$((empWagePerHour*empHr))
+

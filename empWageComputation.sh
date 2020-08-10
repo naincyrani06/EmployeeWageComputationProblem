4a0c8c7908516fd21ad54ec48965dab12446b0c6
naincyrani06 <naincyrani06it@gmail.com>
2020-08-10 21:29:08 +0530

    Calculating Employee Wage Till Total Working Hour Reaches a Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,20 +1,19 @@
 #!/bin/bash -x
 empCheck=$((RANDOM%3))
-
 isPartTime=2
 isFullTime=1
 empWagePerHour=20
 noOfWorkingDays=20
+maximumWorkingHours=100
+totalEmpHour=0
 totalWorkingDays=0
+
 declare -A dailyEmpWage
-while [ $totalWorkingDays -le $noOfWorkingDays ]
+while [[ $totalEmpHour -le $maximumWorkingHours && $totalWorkingDays -le $noOfWorkingDays ]]
 do
-
        ((totalWorkingDays++))
-
        empCheck=$((RANDOM%3))
-
-       case $empCheck in
+	case $empCheck in
                $isPartTime)
                        empHr=4
                ;;
@@ -27,9 +26,11 @@ do
        esac
 
        dailyEmpWage["Day"$totalWorkingDays]=$((empWagePerHour*empHr))
+	totalEmpHour=$((totalEmpHour+empHr))
 done
 for i in "${!dailyEmpWage[@]}"
 do
        echo "$i:${dailyEmpWage[$i]}"
 done
+echo "Total Employee Hours of Employee in a month: "$totalEmpHour
 

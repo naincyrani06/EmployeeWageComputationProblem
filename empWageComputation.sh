954f859448e875810fc2ab06273a859891a51706
naincyrani06 <naincyrani06it@gmail.com>
2020-08-10 21:12:07 +0530

    Calculating Employee Wage For a Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,23 +4,32 @@ empCheck=$((RANDOM%3))
 isPartTime=2
 isFullTime=1
 empWagePerHour=20
+noOfWorkingDays=20
+totalWorkingDays=0
+declare -A dailyEmpWage
+while [ $totalWorkingDays -le $noOfWorkingDays ]
+do
 
-empHr=0
+       ((totalWorkingDays++))
 
-case $empCheck in
-       $isFullTime)
-                echo "Employee is FullTime"
-                empHr=8
-       ;;
-       $isPartTime)
-               echo "Employee is PartTime"
-               empHr=4
-       ;;
-       *)
-               echo "Employee is Absent"
-               empHr=0
-       ;;
-esac
+       empCheck=$((RANDOM%3))
 
-echo "Employee's Daily Wage: "$((empWagePerHour*empHr))
+       case $empCheck in
+               $isPartTime)
+                       empHr=4
+               ;;
+               $isFulltime)
+                       empHr=8
+               ;;
+               *)
+                       empHr=0
+               ;;
+       esac
+
+       dailyEmpWage["Day"$totalWorkingDays]=$((empWagePerHour*empHr))
+done
+for i in "${!dailyEmpWage[@]}"
+do
+       echo "$i:${dailyEmpWage[$i]}"
+done
 
